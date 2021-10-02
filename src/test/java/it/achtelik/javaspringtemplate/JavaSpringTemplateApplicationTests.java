c72d99e94f84cc72dec6999231da966de9d04bfb
Matthias <ma@ma-tech.de>
2021-10-02 23:26:51 +0200

    Init

---
+++ src/test/java/it/achtelik/javaspringtemplate/JavaSpringTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package it.achtelik.javaspringtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
